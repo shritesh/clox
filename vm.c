9e3170cd65a015352d00b4494d1325f12fd0139d
Shritesh Bhattarai <shr@ite.sh>
2021-11-21 20:55:37 -0500

    Global variables

--- vm.c
+++ vm.c
@@ -34,13 +34,16 @@ void initVM()
 {
     resetStack();
     vm.objects = NULL;
+
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM()
 {
-    freeObjects();
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
+    freeObjects();
 }
 
 void push(Value value)
@@ -84,6 +87,7 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op)                        \
     do                                                  \
     {                                                   \
@@ -131,11 +135,44 @@ static InterpretResult run()
         case OP_FALSE:
             push(BOOL_VAL(false));
             break;
+        case OP_POP:
+            pop();
+            break;
+        case OP_GET_GLOBAL:
+        {
+            ObjString *name = READ_STRING();
+            Value value;
+            if (!tableGet(&vm.globals, name, &value))
+            {
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(value);
+            break;
+        }
+        case OP_DEFINE_GLOBAL:
+        {
+            ObjString *name = READ_STRING();
+            tableSet(&vm.globals, name, peek(0));
+            pop();
+            break;
+        }
         case OP_EQUAL:
         {
             Value b = pop();
             Value a = pop();
             push(BOOL_VAL(valuesEqual(a, b)));
+            break;
+        }
+        case OP_SET_GLOBAL:
+        {
+            ObjString *name = READ_STRING();
+            if (tableSet(&vm.globals, name, peek(0)))
+            {
+                tableDelete(&vm.globals, name);
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
             break;
         }
         case OP_GREATER:
@@ -181,14 +218,18 @@ static InterpretResult run()
             }
             push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
-        case OP_RETURN:
+        case OP_PRINT:
             printValue(pop());
             printf("\n");
+            return INTERPRET_OK;
+        case OP_RETURN:
+            // Exit Interpreter
             return INTERPRET_OK;
         }
     }
 
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef READ_BYTE
 #undef BINARY_OP
 }
