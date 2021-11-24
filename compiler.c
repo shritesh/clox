f7f9b50463c3df43eef3b96d63c0d427f1d2216a
Shritesh Bhattarai <shr@ite.sh>
2021-11-24 18:28:51 -0500

    If else

--- compiler.c
+++ compiler.c
@@ -152,6 +152,14 @@ static void emitBytes(uint8_t byte1, uint8_t byte2)
   emitByte(byte2);
 }
 
+static int emitJump(uint8_t instruction)
+{
+  emitByte(instruction);
+  emitByte(0xff);
+  emitByte(0xff);
+  return currentChunk()->count - 2;
+}
+
 static void emitReturn()
 {
   emitByte(OP_RETURN);
@@ -179,6 +187,20 @@ static void initCompiler(Compiler *compiler)
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
   current = compiler;
+}
+
+static void patchJump(int offset)
+{
+  // -2 to adjust for the bytecode for the jump offset itself
+  int jump = currentChunk()->count - offset - 2;
+
+  if (jump > UINT16_MAX)
+  {
+    error("Too much code to jump over.");
+  }
+
+  currentChunk()->code[offset] = (jump >> 8) & 0xff;
+  currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
 static void endCompiler()
@@ -550,6 +572,30 @@ static void expressionStatement()
   emitByte(OP_POP);
 }
 
+static void ifStatement()
+{
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+  int thenJump = emitJump(OP_JUMP_IF_FALSE);
+
+  emitByte(OP_POP);
+
+  statement();
+
+  int elseJump = emitJump(OP_JUMP);
+
+  patchJump(thenJump);
+
+  emitByte(OP_POP);
+
+  if (match(TOKEN_ELSE))
+    statement();
+
+  patchJump(elseJump);
+}
+
 static void printStatement()
 {
   expression();
@@ -607,6 +653,10 @@ static void statement()
   if (match(TOKEN_PRINT))
   {
     printStatement();
+  }
+  else if (match(TOKEN_IF))
+  {
+    ifStatement();
   }
   else if (match(TOKEN_LEFT_BRACE))
   {
