d6a20706d8c901843c24134e485c8fe4d709ef08
Shritesh Bhattarai <shr@ite.sh>
2021-11-28 13:35:30 -0500

    Invocation

--- compiler.c
+++ compiler.c
@@ -548,6 +548,12 @@ static void dot(bool canAssign)
     expression();
     emitBytes(OP_SET_PROPERTY, name);
   }
+  else if (match(TOKEN_LEFT_PAREN))
+  {
+    uint8_t argCount = argumentList();
+    emitBytes(OP_INVOKE, name);
+    emitByte(argCount);
+  }
   else
   {
     emitBytes(OP_GET_PROPERTY, name);
