c44796c70ab260ef1318299649e037935f77772f
Shritesh Bhattarai <shr@ite.sh>
2021-11-28 15:00:54 -0500

    NaN boxing

--- value.c
+++ value.c
@@ -34,6 +34,27 @@ void freeValueArray(ValueArray *array)
 
 void printValue(Value value)
 {
+#ifdef NAN_BOXING
+    if (IS_BOOL(value))
+    {
+        printf(AS_BOOL(value) ? "true" : "false");
+    }
+    else if (IS_NIL(value))
+    {
+
+        printf("nil");
+    }
+    else if (IS_NUMBER(value))
+    {
+
+        printf("%g", AS_NUMBER(value));
+    }
+    else if (IS_OBJ(value))
+    {
+
+        printObject(value);
+    }
+#else
     switch (value.type)
     {
     case VAL_BOOL:
@@ -49,10 +70,18 @@ void printValue(Value value)
         printObject(value);
         break;
     }
+#endif
 }
 
 bool valuesEqual(Value a, Value b)
 {
+#ifdef NAN_BOXING
+    if (IS_NUMBER(a) && IS_NUMBER(b))
+    {
+        return AS_NUMBER(a) == AS_NUMBER(b);
+    }
+    return a == b;
+#else
     if (a.type != b.type)
         return false;
 
@@ -69,4 +98,5 @@ bool valuesEqual(Value a, Value b)
     default:
         return false;
     }
+#endif
 }
