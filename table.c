6302345c9acd77150840221271586c434685edba
Shritesh Bhattarai <shr@ite.sh>
2021-11-28 14:27:59 -0500

    Modulo optimization

--- table.c
+++ table.c
@@ -23,7 +23,7 @@ void freeTable(Table *table)
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
 {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry *tombstone = NULL;
     for (;;)
     {
@@ -45,7 +45,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
             return entry;
         }
 
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -139,7 +139,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
     if (table->count == 0)
         return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     for (;;)
     {
         Entry *entry = &table->entries[index];
@@ -153,7 +153,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
